e0b513282d355d4a20aa72c399985627d5b0fec8
admin <admin@CHETUIWK1860>
2022-05-02 19:56:31 +0530

    Add project files.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ABCDEntities dbContext;
+        public HomeController()
+        {
+            dbContext = new ABCDEntities();
+        }
+
+        public ActionResult Index()
+        {
+            List<Employee> employees = dbContext.Employees.ToList();
+
+            return View(employees);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
